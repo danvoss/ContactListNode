695d6da332da13cdfb2bf791ab81c9aea55fbd1b
Dan <danielandrewvoss@gmail.com>
2016-10-11 13:26:37 -0400

    Add mongo

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,9 +3,22 @@ myApp.controller('AppCtrl', ['$scope', '$http',
   function($scope, $http){
     console.log("Hello world from controller");
 
-    $http.get("/contactlist").success(function(response){
-      console.log("Requested data received.");
-      $scope.contactlist = response;
-    });
+    var refresh = function(){
+      $http.get('/contactlist').success(function(response){
+        console.log("Requested data received.");
+        $scope.contactlist = response;
+        $scope.contact = "";
+      });
+    };
+
+    refresh();
+
+    $scope.addContact = function(){
+      console.log($scope.contact);
+      $http.post('/contactlist', $scope.contact).success(function(response){
+        console.log(response);
+        refresh();
+      });
+    };
 
   }]);
--- server.js
+++ server.js
@@ -1,31 +1,27 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.get("/contactlist", function(req, res){
+app.get('/contactlist', function(req, res){
   console.log("Get request received.");
-  person1 = {
-    name: "Alice",
-    email: "a@gmail.com",
-    number: "555-1212"
-  };
 
-  person2 = {
-    name: "Bob",
-    email: "b@yahoo.com",
-    number: "555-3434"
-  };
+  db.contactlist.find(function(err, docs){
+    console.log(docs);
+    res.json(docs);
+  });
+});
 
-  person3 = {
-    name: "Charlie",
-    email: "c@aol.com",
-    number: "555-5555"
-  };
-
-  var contactlist = [person1, person2, person3];
-  res.json(contactlist);
+app.post('/contactlist', function(req, res){
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err, doc){
+    res.json(doc);
+  });
 });
 
 app.listen(3000);
-console.log("Server running on port 3000");
+console.log("Server running on port 3000.");
